cb91a05b47e2c43a33a9073477d196e10e7debdb
Patrick Reisert <kpreisert@gmail.com>
2016-04-29 14:59:49 +0200

    Run benchmark multiple times, log output and make the number of nodes configurable

--- remote-playground/src/multi-jvm/scala/sae.playground.remote/BenchmarkTest.scala
+++ remote-playground/src/multi-jvm/scala/sae.playground.remote/BenchmarkTest.scala
@@ -1,5 +1,7 @@
 package sae.playground.remote
 
+import java.io.FileOutputStream
+
 import akka.actor
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, ActorPath, Props}
@@ -9,6 +11,8 @@ import idb.BagTable
 import idb.observer.Observer
 import idb.remote._
 
+// change the number of BenchmarkTestMultiJvmNode classes here (by disabling some of the nodes)
+// as well as the number of roles in BenchmarkConfig to configure the benchmark
 class BenchmarkTestMultiJvmNode1 extends BenchmarkTest
 class BenchmarkTestMultiJvmNode2 extends BenchmarkTest
 class BenchmarkTestMultiJvmNode3 extends BenchmarkTest
@@ -34,6 +38,11 @@ with STMultiNodeSpec with ImplicitSender {
   "A RemoteView" must {
     "receive from a simple ObservableHost" in {
       runOn(nodes(0)) {
+        val out: java.io.PrintStream = System.out //new java.io.PrintStream(new FileOutputStream("benchmark.txt", true))
+        out.println("Running benchmark with " + BenchmarkConfig.nodes.size + " nodes ...")
+
+        val beforeInitTime = System.nanoTime()
+
         val db = BagTable.empty[D]
 
         system.actorOf(Props(classOf[ObservableHost[D]], db), "db")
@@ -42,7 +51,7 @@ with STMultiNodeSpec with ImplicitSender {
 
         var tree = RemoteView[D](system, remoteHostPath, false)
 
-        for (i <- 1 to 9) {
+        for (i <- 1 to BenchmarkConfig.nodes.size - 1) {
           tree = RemoteView[D](system, node(nodes(i)).address, tree)
         }
 
@@ -52,7 +61,8 @@ with STMultiNodeSpec with ImplicitSender {
         tree.addObserver(new Observer[D] {
           override def added(v: D) = {
             val currentTime = System.nanoTime()
-            println(currentTime - v)
+            val timeElapsed = (currentTime - v).toDouble / 1000 / 1000
+            out.println("="*50 + "\n** Roundtrip time: " + timeElapsed + "ms\n" + "="*50)
           }
           override def removed(v: D) = {}
           override def updated(oldV: D, newV: D) = {}
@@ -62,17 +72,17 @@ with STMultiNodeSpec with ImplicitSender {
         })
 
         tree.addObserver(new SendToRemote[D](testActor))
+        Thread.sleep(100) // wait until setup complete
 
-        Thread.sleep(100)
+        val afterInitTime = System.nanoTime()
+        out.println("="*50 + "\n** Initialization time: " + ((afterInitTime - beforeInitTime).toDouble / 1000 / 1000) + "ms\n" + "="*50)
 
-        val currentTime = System.nanoTime()
-
-        db += currentTime
-
-        import scala.concurrent.duration._
-        expectMsg(50.seconds, Added(currentTime))
-
-
+        for (i <- 0 to 100) {
+          val currentTime = System.nanoTime()
+          db += currentTime
+          import scala.concurrent.duration._
+          expectMsg(50.seconds, Added(currentTime))
+        }
       }
 
       // needed to keep other hosts running
